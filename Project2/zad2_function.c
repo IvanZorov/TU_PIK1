7daca31fdb0f308a8e37f56c9698e778dcb81487
IvanZorov <i_zoro007@abv.bg>
2015-10-30 19:32:17 +0200

    Update zad2_function.c

--- Project2/zad2_function.c
+++ Project2/zad2_function.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 
 int main(){
-        float a, b, c, m1, m2;
-        float x= 1,y;
-        scanf("%f", &a);
-        scanf("%f", &b);
-        scanf("%f", &c);
-        scanf("%f", &m1);
-        scanf("%f", &m2);
- 
-        if(x<=m1){
-                y = b + x;
-        }
- 
-        if(m1<=x && x<=m2){
-                y = (a*x) / (4 - x);
-        }
- 
-        if(x>=m2){
-                y = c + x;
-        }
- 
-        printf("%f", y);
- 
-        _getche();
-        return 0;
-}
+	float a, b, c, m1, m2;
+	float x = 1, y;
+	scanf_s("%f", &a);
+	scanf_s("%f", &b);
+	scanf_s("%f", &c);
+	scanf_s("%f", &m1);
+	scanf_s("%f", &m2);
 
+	if (x <= m1){
+		y = b + x;
+	}
+
+	if (m1 <= x && x <= m2){
+		y = (a*x) / (4 - x);
+	}
+
+	if (x >= m2){
+		y = c + x;
+	}
+
+	printf("%.2f", y);
+
+	_getche();
+	return 0;
+}
