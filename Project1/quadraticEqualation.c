052664ff56d389a645dd84c984bc7cb7420dd6ef
IvanZorov <i_zoro007@abv.bg>
2015-10-31 12:36:16 +0200

    Update quadraticEqualation.c

--- Project1/quadraticEqualation.c
+++ Project1/quadraticEqualation.c
@@ -3,20 +3,28 @@
 
 int main(){
 
-	float a, b, c, r1, r2, d;
+	float a, b, c, r1, r2, d;  
+	printf("Vavedete stoinosti za a,b i c\n");
 	scanf_s("%f%f%f",&a,&b,&c);
 
-	d = b*b - 4*a*c;
-	if (d > 0){
-		r1 = (-b + sqrt(d)) / (2 * a);
-		r2 = (-b - sqrt(d)) / (2 * a);
-		printf("Korenite sa: x1=%.2f i x2=%.2f",r1,r2);
-	}
-	else if (d = 0){
-		r1 = r2 = -b / (2 * a);
-		printf("Korenite sa ravni: x1=%.2f i x2=%.2f", r1, r2);
+	if (a == 0){
+		r1 = -c / b;
+		printf("Korena e: x=%.2f", r1);
 	} else {
-		printf("Nqma koreni");
+		d = b*b - 4 * a*c;
+
+		if (d > 0){
+			r1 = (-b + sqrt(d)) / (2 * a);
+			r2 = (-b - sqrt(d)) / (2 * a);
+			printf("Korenite sa: x1=%.2f i x2=%.2f", r1, r2);
+		}
+		else if (d == 0){
+			r1 = r2 = -b / (2 * a);
+			printf("Korenite sa ravni: x1=%.2f i x2=%.2f", r1, r2);
+		}
+		else {
+			printf("Nqma koreni");
+		}
 	}
 
 	_getche();
